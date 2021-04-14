c4a1e69662c0fa59a05db0ed4622f774f5ec3801
Priboy313 <Priboy313@yandex.ru>
2021-04-14 20:51:34 +0300

    bd update

--- outer/AutoPic/autopic_db_p.js
+++ outer/AutoPic/autopic_db_p.js
@@ -9,6 +9,7 @@ var Tags = new Map([
 	['ооо бля', 'https://sun9-15.userapi.com/impg/w_JYXlAqZ_DD-XDnvElBw81_XTKiZ0NPHO-dKw/9k6vVGSPit0.jpg?size=1080x1080&quality=96&proxy=1&sign=3d23bd6634c29760a08bf94156b52873&type=album'],
 	['курнул', 'https://sun9-76.userapi.com/impg/tqsO2V7PzUspM11kx8r50l_ay1NqTLXZHHVIpg/570rgAiEZrE.jpg?size=1280x851&quality=96&sign=d50875147f4621db8f18835c6a3bdf5e&type=album'],
 	['ушёлвсебя', 'https://sun9-52.userapi.com/impg/R5j2HrCiaFTki_muU5TvnoBGeXYjICsBrfKowQ/uJpVfaiTV-o.jpg?size=500x500&quality=96&proxy=1&sign=6246d04bf50797595fde2c715cf89cdb&type=album'],
+	['господи', 'https://sun9-37.userapi.com/impf/c840524/v840524208/eff0/3GCaJbYK8m8.jpg?size=594x585&quality=96&sign=fcc24963ebf9021e4aa18e722df33a16&type=album'],
 	['хуита', 'https://sun9-34.userapi.com/impg/UhPAKf9P29wA5JE0YjMF4FBSo-53KOfbxzeyGg/-PHL2AL3Ef4.jpg?size=553x570&quality=96&proxy=1&sign=ecce6ed1ba0e5647b52e0098f48d32db&type=album'],
 	['бонжур', 'https://sun9-53.userapi.com/impg/jP0f2Y-oqlVcugQ7QzCQKEhoZ_DiTyO92Bh79Q/E1olQZFCkZ8.jpg?size=884x525&quality=96&proxy=1&sign=cea26a84b8b5d155d96dfe6b9b042d9a&type=album'],
 	['ересь','https://sun9-58.userapi.com/impg/4YbOSw7xH8hS3lHZTDs4MQLP3SvP21bS-wCo2g/RooLByNQOPk.jpg?size=600x400&quality=96&proxy=1&sign=ffe6d003e6239d4b35fa2d20add44bc7&type=album%27'],
