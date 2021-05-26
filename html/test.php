66c2c58c5ba4f42a60fce41c783f1ebc7f4e58ce
Priboy313 <Priboy313@yandex.ru>
2021-05-26 19:31:05 +0300

    php test 3

--- html/test.php
+++ html/test.php
@@ -1,3 +1,12 @@
-<?php 
-    file_get_contents("/pain.html");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+</head>
+<body>
+    <?php 
+        file_get_contents("/pain.html");
+    ?>
+</body>
