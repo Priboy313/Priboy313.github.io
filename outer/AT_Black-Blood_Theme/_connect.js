18918667bd8710dc6c865784363f216e078a6535
Priboy <priboy313@gmail.com>
2021-11-18 20:31:15 +0300

    AT theme start

---
+++ outer/AT_Black-Blood_Theme/_connect.js
@@ -0,0 +1,11 @@
+
+
+	var head = document.getElementsByTagName("head");
+
+	var css_link = document.createElement("link");
+		css_link.type = "stylesheet";
+		css_link.href = "C:/Users/Priboy/YandexDisk/.github.io/outer/AT_Black-Blood_Theme/AT-BB-Tm.css";
+		
+		head[0].appendChild(css_link);
+		console.log("That's it!");
+
