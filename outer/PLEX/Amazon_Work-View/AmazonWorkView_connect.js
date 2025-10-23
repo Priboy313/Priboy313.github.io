6d625f7e3448920663fc1f55c1c7a7457fd53f97
Priboy313 <Priboy313@yandex.ru>
2025-10-23 14:13:11 +0300

    add script names to logs

--- outer/PLEX/Amazon_Work-View/AmazonWorkView_connect.js
+++ outer/PLEX/Amazon_Work-View/AmazonWorkView_connect.js
@@ -14,70 +14,74 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
 
-    const GITHUB_API_URL = 'https://api.github.com/repos/Priboy313/Priboy313.github.io/commits/main';
-    
-    const SCRIPT_URL_TEMPLATE = 'https://cdn.jsdelivr.net/gh/Priboy313/Priboy313.github.io@{commit_hash}/outer/PLEX/Amazon_Work-View/AmazonWorkView_public.js';
+	const SCRIPT_NAME = "AmznWV";
+
+	const GITHUB_API_URL = 'https://api.github.com/repos/Priboy313/Priboy313.github.io/commits/main';
+	
+	const SCRIPT_URL_TEMPLATE = 'https://cdn.jsdelivr.net/gh/Priboy313/Priboy313.github.io@{commit_hash}/outer/PLEX/Amazon_Work-View/AmazonWorkView_public.js';
 
 	console.log("========== AMZNWV CONNECT (API-driven) ==========");
 
-    GM_xmlhttpRequest({
-        method: 'GET',
-        url: GITHUB_API_URL,
-        headers: {
-            "Accept": "application/vnd.github.v3+json"
-        },
-        onload: function(response) {
-            if (response.status !== 200) {
-                console.error(`[AMZWV] Ошибка при запросе к GitHub API. Статус: ${response.status}`);
-                return;
-            }
+	console.log(`========== ${SCRIPT_NAME} CONNECT (API-driven) ==========`);
 
-            try {
-                const commitInfo = JSON.parse(response.responseText);
-                const latestCommitHash = commitInfo.sha;
+	GM_xmlhttpRequest({
+		method: 'GET',
+		url: GITHUB_API_URL,
+		headers: {
+			"Accept": "application/vnd.github.v3+json"
+		},
+		onload: function(response) {
+			if (response.status !== 200) {
+				console.error(`[${SCRIPT_NAME}] Ошибка при запросе к GitHub API. Статус: ${response.status}`);
+				return;
+			}
 
-                if (!latestCommitHash) {
-                    console.error("Не удалось найти хэш коммита в ответе от GitHub API.");
-                    return;
-                }
+			try {
+				const commitInfo = JSON.parse(response.responseText);
+				const latestCommitHash = commitInfo.sha;
 
-                console.log(`[LOADER] Получен хэш последнего коммита: ${latestCommitHash}`);
+				if (!latestCommitHash) {
+					console.error(`[${SCRIPT_NAME}]Не удалось найти хэш коммита в ответе от GitHub API.`);
+					return;
+				}
 
-                const finalScriptUrl = SCRIPT_URL_TEMPLATE.replace('{commit_hash}', latestCommitHash);
-                downloadAndExecute(finalScriptUrl);
+				console.log(`[${SCRIPT_NAME}] Получен хэш последнего коммита: ${latestCommitHash}`);
 
-            } catch (e) {
-                console.error("Ошибка парсинга ответа от GitHub API:", e);
-            }
-        },
-        onerror: function(error) {
-            console.error("Сетевая ошибка при запросе к GitHub API:", error);
-        }
-    });
+				const finalScriptUrl = SCRIPT_URL_TEMPLATE.replace('{commit_hash}', latestCommitHash);
+				downloadAndExecute(finalScriptUrl);
 
-    function downloadAndExecute(url) {
-        console.log(`[LOADER] Загрузка скрипта с URL: ${url}`);
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: url,
-            onload: function(res) {
-                if (res.status === 200 && res.responseText) {
-                    console.log("========== RESPONSE SUCCSESS. EXECUTING SCRIPT...");
-                    try {
-                        eval(res.responseText);
-                    } catch (e) {
-                        console.error("========== EVAL FAILED:", e);
-                    }
-                } else {
-                    console.error(`Ошибка загрузки скрипта. Статус: ${res.status}`);
-                }
-            },
-            onerror: function(error) {
-                console.error("Сетевая ошибка при загрузке скрипта:", error);
-            }
-        });
-    }
+			} catch (e) {
+				console.error(`[${SCRIPT_NAME}] Ошибка парсинга ответа от GitHub API:`, e);
+			}
+		},
+		onerror: function(error) {
+			console.error(`[${SCRIPT_NAME}] Сетевая ошибка при запросе к GitHub API:`, error);
+		}
+	});
+
+	function downloadAndExecute(url) {
+		console.log(`[${SCRIPT_NAME}] Загрузка скрипта с URL: ${url}`);
+		GM_xmlhttpRequest({
+			method: 'GET',
+			url: url,
+			onload: function(res) {
+				if (res.status === 200 && res.responseText) {
+					console.log(`========== RESPONSE SUCCSESS. EXECUTING [${SCRIPT_NAME}] SCRIPT...`);
+					try {
+						eval(res.responseText);
+					} catch (e) {
+						console.error("========== EVAL FAILED:", e);
+					}
+				} else {
+					console.error(`[${SCRIPT_NAME}] Ошибка загрузки скрипта. Статус: ${res.status}`);
+				}
+			},
+			onerror: function(error) {
+				console.error(`[${SCRIPT_NAME}] Сетевая ошибка при загрузке скрипта:`, error);
+			}
+		});
+	}
 
 })();
