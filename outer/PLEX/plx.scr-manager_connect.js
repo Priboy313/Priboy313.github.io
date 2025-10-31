706d18a3a5c214a8bcf704e1ce71c513e2d0f65d
Priboy313 <Priboy313@yandex.ru>
2025-10-31 16:23:35 +0300

    fix icon

--- outer/PLEX/plx.scr-manager_connect.js
+++ outer/PLEX/plx.scr-manager_connect.js
@@ -11,7 +11,7 @@
 // @grant        GM_addStyle
 // @connect      cdn.jsdelivr.net
 // @connect      api.github.com
-// @icon https://twemoji.maxcdn.com/v/latest/72x72/2699.png
+// @ico          https://twemoji.maxcdn.com/v/latest/72x72/2699.png
 // ==/UserScript==
 
 (function() {
